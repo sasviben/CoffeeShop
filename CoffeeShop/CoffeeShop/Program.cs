a16084e1e789269451915b2fdc691987d3cee5d0
sandro sviben <sandro.sviben@hotmail.com>
2017-02-16 22:30:10 +0100

    Switch case checked Do While loop chacked

---
+++ CoffeeShop/CoffeeShop/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int UserChoice = 0;
+            var CoffeeCost = 0;
+            string UserDecision = string.Empty;
+            do
+            {
+
+
+                do
+                {
+                    Console.WriteLine("Please select your coffee size : 1 - Small, 2 - Medium, 3 - Large");
+                    UserChoice = int.Parse(Console.ReadLine());
+                    //TODO exception handling
+                    switch (UserChoice)
+                    {
+                        case 1:
+                            CoffeeCost += 1;
+                            break;
+                        case 2:
+                            CoffeeCost += 2;
+                            break;
+                        case 3:
+                            CoffeeCost += 3;
+                            break;
+                        default:
+                            Console.WriteLine("Your choice {0} is invalid. Please try again...", UserChoice);
+                            break;
+                    }
+
+                } while (UserChoice != 1 && UserChoice != 2 && UserChoice != 3);
+
+                do
+                {
+
+
+                    Console.WriteLine("Do you want to buy another coffee? - Yes or No");
+                    //upper case?
+                    UserDecision = Console.ReadLine();
+                    //TODO exception handling
+                    if (UserDecision != "Yes" && UserDecision != "No")
+                    {
+                        Console.WriteLine("Your choice {0} is invalid.Please try again...", UserDecision);
+                    }
+                } while (UserDecision != "Yes" && UserDecision != "No");
+            } while (UserDecision != "No");
+            Console.WriteLine("Thank you for shoping with us.\nBill amonut is {0}", CoffeeCost);
+        }
+    }
+}
